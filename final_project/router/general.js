1743e82b7b37e6a8e4a919b62b84c902fbff636d
kai-codes96 <Kayinwalker16@Gmail.com>
2025-01-31 21:45:04 -0500

    bookReviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
@@ -24,14 +23,28 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const authorAuth = author.filter((authors) => authors.author === author)
+
+    if (req.params.author) {
+        authorAuth = author;
+        res.send(`The author is ${author}`);
+    } else {
+        res.send("Book not available...stupid");
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const titleAuth = title.filter((titles) => titles.title === title);
+
+    if (req.params.title) {
+        titleAuth = title;
+        res.send(`The title of the book is ${title}`);
+    } else {
+        res.send("What...what book is that?");
+    }
 });
 
 //  Get book review
